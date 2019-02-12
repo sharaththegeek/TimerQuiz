46ca9008134e6080f663b56aa3be506f1b75d673
Sharath <sharath0698@gmail.com>
2019-02-12 21:52:54 +0530

    Defining backend serializers

--- backend/timer_backend/quiz/serializers.py
+++ backend/timer_backend/quiz/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
 from .models import Question
+from .models import Participant
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model=Question
         fields=('id','question','option1','option2','option3','option4')
+
+class ParticipantSerializer(serializers.ModelSerializer):
+    teamid=serializers.CharField(required=True,validators=[UniqueValidator(queryset=Participant.objects.all())])
+    name1=serializers.CharField(max_length=60)
+    name2=serializers.CharField(max_length=50)
+
+    def create(self,validated_data):
+        participant=Participant(teamid=validated_data['teamid'],name1=name1,name2=name2,score=0)
+        participant.save()
+        return participant
+
+    class Meta:
+        model=Participant
+        fields=('teamid','name1','name2','score')
