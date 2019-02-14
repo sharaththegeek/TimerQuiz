025a1de496855630558b9d23460838417379a0e8
Sharath <sharath0698@gmail.com>
2019-02-15 00:20:47 +0530

    Cleaning

--- backend/timer_backend/quiz/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Question
-from .models import Participant
-
-admin.site.register(Participant)
-admin.site.register(Question)
--- backend/timer_backend/quiz/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Participant
-
-class CreateParticipant(forms.Form):
-    teamid=forms.CharField(max_length=20)
-    name1=forms.CharField(max_length=60)
-    name2=forms.CharField(max_length=50)
-    score=forms.IntegerField()
